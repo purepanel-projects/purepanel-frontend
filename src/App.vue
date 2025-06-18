104db83a84c9535e35ee3c83be3086c0a38cb14d
zyan1226 <zyan1226@163.com>
2025-06-19 00:04:08 +0800

    登录界面完善

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@ import {RouterView, useRouter} from 'vue-router'
 const router = useRouter()
 
 if (localStorage.getItem("token")) {
-  router.replace("/")
+  // router.replace("/")
 } else {
   router.replace("/login")
 }
---
+++ src/pages/login/component/LoginHeader.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="h-14 absolute top-0 w-full backdrop-blur-md
+    flex flex-row items-center px-6">
+    <img class="w-24" src="@/assets/logo-h.png" alt="logo">
+    <div class="ml-auto">
+      <a target="_blank" href="https://github.com/purepanel-projects">
+        <t-button variant="text">
+          <template #icon>
+            <logo-github-icon/>
+          </template>
+        </t-button>
+      </a>
+      <a target="_blank" href="https://github.com/purepanel-projects">
+        <t-button variant="text">
+          <template #icon>
+            <help-circle-icon/>
+          </template>
+        </t-button>
+      </a>
+    </div>
+  </div>
+</template>
+<script setup lang="ts">
+import {LogoGithubIcon, HelpCircleIcon} from "tdesign-icons-vue-next";
+</script>
