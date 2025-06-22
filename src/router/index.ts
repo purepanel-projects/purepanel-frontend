8ecf41accc17bf323e9f523e2c38ac9b837a4f3b
zyan1226 <zyan1226@163.com>
2025-06-22 22:46:11 +0800

    添加nprogress

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,6 +2,8 @@ import {createRouter, createWebHistory} from 'vue-router'
 import Layout from '@/pages/layout/Layout.vue'
 import LoginPage from "@/pages/login/LoginPage.vue";
 import UserList from "@/pages/system/user/UserList.vue";
+import NProgress from 'nprogress'
+import 'nprogress/nprogress.css'
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
@@ -32,6 +34,20 @@ const router = createRouter({
         //   component: () => import('../views/AboutView.vue'),
         // },
     ],
+})
+
+//配置 NProgress
+NProgress.configure({showSpinner: false})
+router.beforeEach((to, from, next) => {
+    console.log('NProgress')
+    NProgress.start()
+    next()
+})
+router.afterEach(() => {
+    console.log('NProgress')
+    setTimeout(() => {
+        NProgress.done()
+    }, 100)
 })
 
 export default router
