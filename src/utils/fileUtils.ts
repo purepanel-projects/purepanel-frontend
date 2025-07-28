250f4654768235b3296beb8af1a5189ad4b899b8
zyan1226 <zyan1226@163.com>
2025-07-28 17:18:27 +0800

    小优化

--- src/utils/fileUtils.ts
+++ src/utils/fileUtils.ts
@@ -1,4 +1,4 @@
 //根据文件相对路径获取文件网络地址
 export function getFileNetworkPath(filePath: string) {
-    return `${import.meta.env.VITE_BASE_API}/files${filePath}`;
+    return `${import.meta.env.VITE_BASE_API}/files/${filePath}`;
 }
