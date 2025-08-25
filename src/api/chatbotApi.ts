b414f8f02c00bfec45795eac1a7957505568023f
zyan1226 <zyan1226@163.com>
2025-08-25 16:10:13 +0800

    新增查询AI聊天记录接口

--- src/api/chatbotApi.ts
+++ src/api/chatbotApi.ts
@@ -2,7 +2,14 @@
 import type Res from "@/types/Res.ts";
 import type {AiChatbotConversation} from "@/types/AiChatbotConversation.ts";
 import request from "@/api/request.ts";
+import type {AiChatbotChatRecord} from "@/types/AiChatbotChatRecord.ts";
 
+//获取会话列表
 export function listChatbotConversationApi(): Promise<Res<AiChatbotConversation[]>> {
     return request.get('/chatbot/listConversation')
+}
+
+//获取会话记录
+export function listChatbotChatRecordApi(conversationId: string): Promise<Res<AiChatbotChatRecord[]>> {
+    return request.get(`/chatbot/listChatRecord?conversationId=${conversationId}`)
 }
