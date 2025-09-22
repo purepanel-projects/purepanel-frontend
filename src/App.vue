a5c71101e8368ae9e477ed39aadce52198af9c23
zyan1226 <zyan1226@163.com>
2025-09-22 13:54:42 +0800

    主题色可通过func切换

--- src/App.vue
+++ src/App.vue
@@ -4,11 +4,14 @@
 
 <script setup lang="ts">
 import {RouterView} from 'vue-router'
+import {setThemeColor} from "@/utils/dynamicThemes.ts";
 
 const themeMode = localStorage.getItem('theme-mode')
 if (themeMode === 'dark') {
   document.documentElement.setAttribute("theme-mode", "dark");
 }
+//默认主题
+setThemeColor('purple')
 </script>
 
 <style scoped>
